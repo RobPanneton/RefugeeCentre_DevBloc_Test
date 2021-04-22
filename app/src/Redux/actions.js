fc05c8150052ebad09842748d5f33338ac194268
RobPanneton <robert.panneton@gmail.com>
2021-04-21 20:50:44 -0400

    add redux actions

--- app/src/Redux/actions.js
+++ app/src/Redux/actions.js
@@ -2,3 +2,37 @@ export const populateInventory = (item) => ({
   type: "POPULATE_INVENTORY",
   payload: item,
 });
+
+export const ChangeCategory = (category) => ({
+  type: "CHANGE_CATEGORY",
+  payload: category,
+});
+
+export const addToCart = (item) => ({
+  type: "ADD_TO_CART",
+  payload: item,
+});
+
+export const addToCartWithQuantity = (item, quantity) => ({
+  type: "ADD_TO_CART_WITH_QUANTITY",
+  payload: { item, quantity },
+});
+
+export const IncrementQuantity = (item) => ({
+  type: "INCREMENT_QUANTITY",
+  payload: item,
+});
+
+export const DecrementQuantity = (item) => ({
+  type: "DECREMENT_QUANTITY",
+  payload: item,
+});
+
+export const removeFromCart = (item) => ({
+  type: "REMOVE_FROM_CART",
+  payload: item,
+});
+
+export const emptyCart = () => ({
+  type: "EMPTY_CART",
+});
